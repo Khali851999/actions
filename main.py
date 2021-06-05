2e0478574fa0d06302c8eae28509dcd61af8fa62
Khali851999 <akshatjindal215@gmail.com>
2021-06-05 23:57:57 +0530

    init

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-imp
+import urllib, json
+
+url  = "https://raw.githubusercontent.com/Khali851999/actions/main/t.json"
+response = urllib.urlopen(url)
+req = response.read()
+data = json.loads(req.decode("utf-8"))
+print (data)
+
+with open("Readme.md") as file:
+    file.write(data['x'])
